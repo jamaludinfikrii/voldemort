7ea0ff31e30e311c2d4732a94260124ca0e6d400
fiqri <jamaludinfikrii@gmail.com>
2021-10-10 21:29:51 +0700

    session 3

---
+++ 1.3.sharing_session.js
@@ -0,0 +1,91 @@
+// QNA
+
+// - Review segitiga ?
+// - Interview kerjaan bagemana ? Nanti diajarin ketika kalian udah nyampe functions
+// - Github bisa copas copas project lain ? Betull, cuman tidak semudah itu
+// - Bootstrap ? bikin kerjaan frontend lebih gampang, salah satu feature nya responsive
+// - Tutorial hell ? Perbanyak praktek
+// - Not secure website itu apa ? website yang transfer datanya gak di encrypt, jadi bisa kena sama Man in the middle attack
+// - Latihan latihan program ada website gratis ? banyak, ada WPU, Fcc, Codewars, hackerrank, belajar sampe function dulu, baru belajar soal soal
+
+
+// ACTION ITEM
+// vote di group tentang bahas exercise
+// Bikin group discord / slack ?
+// hosting dan domain di provide gak ?
+
+
+// MATERI
+// Review Segitiga
+
+// for(let i = 1 ; i <= 3 ; i ++) { // gak pake \n, karena kita panggil console.log 3 kali, setiap manggil console.log, udah otomatis next line
+//   console.log('*'.repeat(i))
+// }
+
+
+// result = '*\n**\n***\n'
+// var result = ''
+
+// for(let p = 1 ; p <= 3 ; p ++) { // += (nambah value yang lama dengan value baru)
+
+//   for(let t = 0; t < p ; t ++) {
+//     result += '*'
+//   }
+//   result += '\n'
+
+// }
+
+// console.log(result)
+
+// result = '*\n**\n***\n'
+// var result = ''
+
+// // looping for, increment setelah action, check kondisi ==> action ==> increment
+// for(let p = 1 ; p <= 5 ; p ++) { // += (nambah value yang lama dengan value baru)
+//   result += '*'
+// }
+
+
+// console.log(result) //
+
+
+// p = 1 || 2 || 3 || 4 || 5 || 6
+// result = '*****'
+
+
+// for(var jeje = 100 ; jeje < 90 ; jeje ++) {
+//   console.log('aaaaaaaaaaaaa')
+// }
+
+// for(var i = 5 ; i <= 10 ; i ++) {
+//   console.log(i)
+// }
+
+
+// var x = 10
+// do {
+//   x -= 1
+// } while (x > 0)
+
+
+// var input = 'Jamaludin Fiqri'
+
+// for(var i = 0 ; i < input.length ; i ++) {
+//   var huruf = input[i]
+//   console.log(huruf + ' = ' + i)
+// }
+
+
+
+// var input = 'Jamaludin Fiqri Adalah'
+// var jumlahA = 0
+// for(var i = 0 ; i < input.length ; i ++) {
+//   var huruf = input[i]
+
+//   if(huruf === 'a' || huruf === 'A') {
+//     jumlahA ++
+//   }
+
+// }
+
+// console.log(jumlahA)
